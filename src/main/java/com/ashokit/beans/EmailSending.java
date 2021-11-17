996e434939a1ec8f1afbe435b765c64064e5ecfc
Niketan Thombare <nickthombare@gmail.com>
2021-11-17 21:08:40 +0530

    chnages in Email Sending

--- src/main/java/com/ashokit/beans/EmailSending.java
+++ src/main/java/com/ashokit/beans/EmailSending.java
@@ -1,8 +1,13 @@
 package com.ashokit.beans;
 
+import java.util.Properties;
+
+import org.springframework.boot.web.servlet.server.Session;
+
 public class EmailSending {
      public void needToSend()
      {
-    	 //
+    	
+    
      }
 }
