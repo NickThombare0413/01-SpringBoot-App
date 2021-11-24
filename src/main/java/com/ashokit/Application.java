982d11370b920baf46e1f228f781ddc80e1711e1
Niketan Thombare <nickthombare@gmail.com>
2021-11-24 23:45:41 +0530

    HIS-301 realted changes

--- src/main/java/com/ashokit/Application.java
+++ src/main/java/com/ashokit/Application.java
@@ -5,9 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
-
+private map<String,Object>cache=new Hashmap<String,Object>();
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
+
+public void loadDataToCache()
+{
+}
 
 }
